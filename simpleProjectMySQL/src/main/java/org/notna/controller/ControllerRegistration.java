345c5c87def03aaeedf9e65f4f754597f108eb3d
notnaGER <75083505+notnaGER@users.noreply.github.com>
2021-02-14 00:20:33 +0100

    dev class

--- simpleProjectMySQL/src/main/java/org/notna/controller/ControllerRegistration.java
+++ simpleProjectMySQL/src/main/java/org/notna/controller/ControllerRegistration.java
@@ -1,6 +1,8 @@
 package org.notna.controller;
 
 import java.net.URL;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.ResourceBundle;
 
 import org.notna.model.Student;
@@ -12,6 +14,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.input.MouseEvent;
 
 public class ControllerRegistration implements Initializable {
 
@@ -34,46 +39,67 @@ public class ControllerRegistration implements Initializable {
     private Button btn_edit;
 
     @FXML
-    private TableColumn<?, ?> column_id;
+    private TableColumn<Student, Integer> column_id;
 
     @FXML
-    private TableColumn<?, ?> column_name;
+    private TableColumn<Student, String> column_name;
 
     @FXML
-    private TableColumn<?, ?> column_mobile;
+    private TableColumn<Student, String> column_mobile;
 
     @FXML
-    private TableColumn<?, ?> column_course;
+    private TableColumn<Student, String> column_course;
 
     @FXML
     private TableView<Student> tableViewStudent;
 
     @FXML
+    void addClickAction(MouseEvent event) {
+    	this.tableViewStudent.getSelectionModel().clearSelection();
+    }
+
+    @FXML
     void handleAddAction(ActionEvent event) {
     	this.tableViewStudent.getItems().add(new Student(this.txf_name.getText(), this.txf_mobile.getText(), this.txf_coure.getText()));
+    	Arrays.asList(this.txf_name,this.txf_mobile,this.txf_coure).stream().forEach(txf->txf.setText(""));
     }
 
     @FXML
     void handleDeleteAction(ActionEvent event) {
-
+    	this.tableViewStudent.getItems().remove(this.tableViewStudent.getSelectionModel().getSelectedItem());
+    	Arrays.asList(this.txf_name,this.txf_mobile,this.txf_coure).stream().forEach(txf->txf.setText(""));
     }
 
     @FXML
     void handleEditAction(ActionEvent event) {
+    	Student oldValue = this.tableViewStudent.getSelectionModel().getSelectedItem();
+    	Collections.replaceAll(this.tableViewStudent.getItems(), oldValue , new Student(this.txf_name.getText(), this.txf_mobile.getText(), this.txf_coure.getText()));
+
+    }
+
+    @FXML
+    void handleChoiceAction(MouseEvent event) {
+    	if(!this.tableViewStudent.getSelectionModel().isEmpty()){
+    		Student chooseStudent = this.tableViewStudent.getSelectionModel().getSelectedItem();
+        	this.txf_name.setText(chooseStudent.getName());
+        	this.txf_mobile.setText(chooseStudent.getMobileNr());
+        	this.txf_coure.setText(chooseStudent.getCourse());
+    	}
 
     }
 
 	public void initialize(URL location, ResourceBundle resources) {
-		this.tableViewStudent = new TableView<Student>();
-		this.column_id = new TableColumn<Student, Integer>();
-	    this.column_name = new TableColumn<Student, String>();
-	    this.column_mobile = new TableColumn<Student, String>();
-	    this.column_course = new TableColumn<Student, String>();
-	    this.getStudentData();
-
+		this.getStudentData();
+	    this.column_id.setCellValueFactory(new PropertyValueFactory<Student,Integer>("id"));
+	    this.column_name.setCellValueFactory(new PropertyValueFactory<Student,String>("name"));
+	    this.column_mobile.setCellValueFactory(new PropertyValueFactory<Student,String>("mobileNr"));
+	    this.column_course.setCellValueFactory(new PropertyValueFactory<Student,String>("course"));
+	    this.column_name.setCellFactory(TextFieldTableCell.forTableColumn());
+	    this.column_mobile.setCellFactory(TextFieldTableCell.forTableColumn());
+	    this.column_course.setCellFactory(TextFieldTableCell.forTableColumn());
 	}
 
 	private void getStudentData(){
-		this.tableViewStudent.getItems().addAll(/*get Student from DB*/);
+		this.tableViewStudent.getItems().addAll(new Student("Peter", "01564", "php"));
 	}
 }
---
+++ simpleProjectMySQL/src/main/java/org/notna/main/ControllerRegistration.java
@@ -0,0 +1,79 @@
+package org.notna.main;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import org.notna.model.Student;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class ControllerRegistration implements Initializable {
+
+    @FXML
+    private TextField txf_coure;
+
+    @FXML
+    private TextField txf_mobile;
+
+    @FXML
+    private TextField txf_name;
+
+    @FXML
+    private Button btn_add;
+
+    @FXML
+    private Button btn_delete;
+
+    @FXML
+    private Button btn_edit;
+
+    @FXML
+    private TableColumn<?, ?> column_id;
+
+    @FXML
+    private TableColumn<?, ?> column_name;
+
+    @FXML
+    private TableColumn<?, ?> column_mobile;
+
+    @FXML
+    private TableColumn<?, ?> column_course;
+
+    @FXML
+    private TableView<Student> tableViewStudent;
+
+    @FXML
+    void handleAddAction(ActionEvent event) {
+    	this.tableViewStudent.getItems().add(new Student(this.txf_name.getText(), this.txf_mobile.getText(), this.txf_coure.getText()));
+    }
+
+    @FXML
+    void handleDeleteAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleEditAction(ActionEvent event) {
+
+    }
+
+	public void initialize(URL location, ResourceBundle resources) {
+		this.tableViewStudent = new TableView<Student>();
+		this.column_id = new TableColumn<Student, Integer>();
+	    this.column_name = new TableColumn<Student, String>();
+	    this.column_mobile = new TableColumn<Student, String>();
+	    this.column_course = new TableColumn<Student, String>();
+	    this.getStudentData();
+
+	}
+
+	private void getStudentData(){
+		this.tableViewStudent.getItems().addAll(/*get Student from DB*/);
+	}
+}
