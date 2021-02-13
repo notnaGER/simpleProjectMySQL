4d820379e86f8284d988431b3ca37464c599335c
notnaGER <75083505+notnaGER@users.noreply.github.com>
2021-02-13 20:54:34 +0100

    create add student action

--- simpleProjectMySQL/src/main/java/org/notna/controller/ControllerRegistration.java
+++ simpleProjectMySQL/src/main/java/org/notna/controller/ControllerRegistration.java
@@ -1,11 +1,19 @@
 package org.notna.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import org.notna.model.Student;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
-public class ControllerRegistration {
+public class ControllerRegistration implements Initializable {
 
     @FXML
     private TextField txf_coure;
@@ -37,4 +45,35 @@ public class ControllerRegistration {
     @FXML
     private TableColumn<?, ?> column_course;
 
+    @FXML
+    private TableView<Student> tableViewStudent;
+
+    @FXML
+    void handleAddAction(ActionEvent event) {
+    	this.tableViewStudent.getItems().add(new Student(this.txf_name.getText(), this.txf_mobile.getText(), this.txf_coure.getText()));
+    }
+
+    @FXML
+    void handleDeleteAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleEditAction(ActionEvent event) {
+
+    }
+
+	public void initialize(URL location, ResourceBundle resources) {
+		this.tableViewStudent = new TableView<Student>();
+		this.column_id = new TableColumn<Student, Integer>();
+	    this.column_name = new TableColumn<Student, String>();
+	    this.column_mobile = new TableColumn<Student, String>();
+	    this.column_course = new TableColumn<Student, String>();
+	    this.getStudentData();
+
+	}
+
+	private void getStudentData(){
+		this.tableViewStudent.getItems().addAll(/*get Student from DB*/);
+	}
 }
