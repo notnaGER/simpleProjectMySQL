45ad43c0b6b4c705c26523023d82cd2bcc1a9251
notnaGER <75083505+notnaGER@users.noreply.github.com>
2021-02-14 23:58:48 +0100

    create class DbConnection

---
+++ simpleProjectMySQL/src/main/java/org/notna/database/DbConnection.java
@@ -0,0 +1,33 @@
+package org.notna.database;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnection {
+	private static Connection connection;
+
+	private DbConnection(){
+
+	}
+
+	private static void createConnection() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager
+					.getConnection("jdbc:mysql://localhost:3306/programming_class","root","");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static Connection getConnection() {
+		if (connection == null) {
+			createConnection();
+		}
+		return connection;
+	}
+}
